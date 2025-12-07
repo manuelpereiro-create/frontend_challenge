285609c2a2f0f2e3689ba4a4a3570b2a2ad62f20
Manuel Pereiro <manuel.pereiro@globant.com>
2025-12-07 14:52:03 -0300

    adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./features/login/login.component";
+import { RegisterComponent } from "./features/register/register.component";
+import { DashboardComponent } from "./features/dashboard/dashboard.component";
+import { authGuard } from "./core/guards/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    // protected routes
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [authGuard]
+    },
+    {path: '**', redirectTo: 'login'} // unknown routes redirect to login
+];
